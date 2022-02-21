b2d46c6a232a9111f8125e63926e317ec2a7bd06
Prasanth <Prasanth@DESKTOP-LA9THFC>
2022-02-21 12:36:33 +0530

    Added additonal checks

--- src/firstProgram/FirstDemo2.java
+++ src/firstProgram/FirstDemo2.java
@@ -4,7 +4,7 @@ public class FirstDemo2 {
 
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if (hungry == "yes")
+		if (hungry == "yes" || hungry =="Y")
 			System.out.println("Eat something....");
 		else {
 			System.out.println("Do work .....");
